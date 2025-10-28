0e59c403099bd41c01aa869233255288dc637846
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-10-28 23:28:44 +0530

    Feat: INDEX route with minimal ejs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import path from "path";
 import express from "express";
 import mongoose from "mongoose";
 import Listings from "./models/listing.js";
@@ -8,6 +9,7 @@ const port = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "views"));
 
 main()
   .then(res => console.log(`connected to wanderlust DB`))
@@ -21,7 +23,8 @@ app.listen(port, () => {
   console.log(`Listening at port no. ${port}`);
 });
 
+// INDEX route
 app.get("/listings", async (req, res) => {
   const allListings = await Listings.find();
-  res.render("index.ejs", { allListings });
+  res.render("./listings/index.ejs", { allListings });
 });
