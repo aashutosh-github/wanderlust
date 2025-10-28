62e6e1e311286d649cef86d70a3d4f43a27c82cc
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-10-28 23:14:22 +0530

    Feat: GET route for index.ejs

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import Listings from "./models/listing.js";
 
 const app = express();
 const port = process.env.PORT;
-
 const MONGO_URL = process.env.MONGO_URL;
+
+app.set("view engine", "ejs");
 
 main()
   .then(res => console.log(`connected to wanderlust DB`))
@@ -17,4 +19,9 @@ async function main() {
 
 app.listen(port, () => {
   console.log(`Listening at port no. ${port}`);
+});
+
+app.get("/listings", async (req, res) => {
+  const allListings = await Listings.find();
+  res.render("index.ejs", { allListings });
 });
